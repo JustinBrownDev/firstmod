17beed14dce20f348d73ba150c48578667eac190
JustinBrownDev <justin@jweb.dev>
2025-10-01 17:31:05 -0500

    Got it to work added assets to client, added model description

--- src/main/java/name/modid/item/ModItems.java
+++ src/main/java/name/modid/item/ModItems.java
@@ -1,16 +1,43 @@
 package name.modid.item;
 import name.modid.FirstMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
+
 public class ModItems {
-    public static final Item anItem = registerItem("firstitem", new Item(new Item.Settings()));
-    private static Item registerItem(String name, Item item){
-        return Registry.register(Registries.ITEM, Identifier.of(FirstMod.MOD_ID,name),item);
+    public static final Item firstitem = register("firstitem", Item::new, new Item.Settings());
+    //public static final Item ANOTHER_ITEM = registerItem("seconditem", new Item(new Item.Settings()));
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(FirstMod.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
     }
+//    private static Item registerItem(String nam){
+//        Identifier id = Identifier.of(FirstMod.MOD_ID,nam);
+//        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+//        Item.Settings settings = new Item.Settings().registryKey(key);
+//        return Registry.register(Registries.ITEM, key,new Item(settings));
+//    }
+
     public static void registerModItems(){
         FirstMod.LOGGER.info("Registering Mod Items for " + FirstMod.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(ModItems.firstitem);
+            //entries.add(ANOTHER_ITEM);
+        });
     }
 }
