e1d8af2a9655307fd4b8945bb188b1d0f79829cd
JustinBrownDev <justin@jweb.dev>
2025-10-03 11:15:46 -0500

    changed name of item to compact_disc (lowercase)

--- src/main/java/name/modid/item/ModItems.java
+++ src/main/java/name/modid/item/ModItems.java
@@ -12,15 +12,13 @@ import net.minecraft.util.Identifier;
 import java.util.function.Function;
 
 public class ModItems {
-    public static final Item Compact_Disc = register("Compact_Disc", Item::new, new Item.Settings());
+    public static final Item compact_disc = register("compact_disc", Item::new, new Item.Settings());
     //public static final Item ANOTHER_ITEM = registerItem("seconditem", new Item(new Item.Settings()));
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         // Create the item key.
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(FirstMod.MOD_ID, name));
-
         // Create the item instance.
         Item item = itemFactory.apply(settings.registryKey(itemKey));
-
         // Register the item.
         Registry.register(Registries.ITEM, itemKey, item);
 
@@ -30,7 +28,7 @@ public class ModItems {
     public static void registerModItems(){
         FirstMod.LOGGER.info("Registering Mod Items for " + FirstMod.MOD_ID);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(ModItems.Compact_Disc);
+            entries.add(ModItems.compact_disc);
             //entries.add(ANOTHER_ITEM);
         });
     }
